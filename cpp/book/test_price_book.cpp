90d346d279c443f8e29aa3fbec5463f80ac0a132
Yesmurat Sagyndyk <yesmurat2006@gmail.com>
2026-09-04 16:33:15 +0500

    order book test: added test_increment_existing_level() test function.

--- cpp/book/test_price_book.cpp
+++ cpp/book/test_price_book.cpp
@@ -15,10 +15,26 @@ void test_increment_new_level() {
 
 }
 
+void test_increment_existing_level() {
+
+    book::PriceLevel level0 {250, 123450};
+    book::PriceBook book0{true};
+
+    book0.increment(level0.price, level0.aggregate_shares);
+    auto result = book0.increment(level0.price, level0.aggregate_shares);
+
+    assert(result == book::PriceBook::UpdateResult::Ok);
+    assert(book0.count() == 1);
+    assert(book0.data()[0].price == level0.price);
+    assert(book0.data()[0].aggregate_shares == (level0.aggregate_shares * 2));
+
+}
+
 int main(int argc, char** argv) {
 
     test_increment_new_level();
-    
+    test_increment_existing_level();
+
     puts("All Price Book tests passed.\n");
     return 0;
 }
