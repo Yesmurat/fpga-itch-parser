63f0943a074754c082386f7fc88bb1c0e772ef8b
Yesmurat Sagyndyk <yesmurat2006@gmail.com>
2026-09-04 14:12:05 +0500

    price book: finished and tested decrement() function.

--- cpp/book/price_book.hpp
+++ cpp/book/price_book.hpp
@@ -15,7 +15,7 @@ namespace book {
         public:
             static constexpr size_t CAPACITY = 512;
 
-            enum class UpdateResult {Ok, Full};
+            enum class UpdateResult {Ok, Full, NotFound, InvalidDecrement};
 
             PriceBook(bool is_buy_side) : is_buy_{is_buy_side} {}
 
@@ -73,7 +73,64 @@ namespace book {
 
             }
 
-            UpdateResult decrement(uint32_t price, uint32_t shares) {}
+            UpdateResult decrement(uint32_t price, uint32_t shares) {
+
+                int lowerbound = 0;
+                int upperbound = count_ - 1;
+
+                if (count_ != 0) {
+
+                    while (lowerbound <= upperbound) {
+
+                        int midPoint = lowerbound + (upperbound - lowerbound) / 2;
+                    
+                        if (levels_[midPoint].price == price) { // Target found at midPoint.
+
+                            // if shares to subtract is bigger than aggregate shares -> report InvalidDecrement.
+                            if (shares > levels_[midPoint].aggregate_shares) {
+                                return UpdateResult::InvalidDecrement;
+                            }
+
+                            levels_[midPoint].aggregate_shares -= shares;
+
+                            if (levels_[midPoint].aggregate_shares > 0) {
+                                return UpdateResult::Ok;
+                            }
+
+                            else if (levels_[midPoint].aggregate_shares == 0) {
+
+                                // if shares is 0 -> remove this slot from the array.
+                                for (size_t i = midPoint + 1; i <= count_-1; i++)
+                                    levels_[i-1] = levels_[i]; // shift the elements
+
+                                count_--;
+                                return UpdateResult::Ok;
+
+                            }
+
+                        }
+                    
+                        else {
+
+                            if (is_better(levels_[midPoint].price, price)) {
+                                lowerbound = midPoint + 1;
+                            }
+                        
+                            else {
+                                upperbound = midPoint - 1;
+                            }
+
+                        }
+
+                    }
+
+                }
+
+                // not found.
+                // something asked to remove from a price this PriceBook was never tracking.
+                return UpdateResult::NotFound;
+
+            }
 
         private:
             size_t count_ = 0; // how many of slots are actually in use; everything from count_ onward is stale/unused.
