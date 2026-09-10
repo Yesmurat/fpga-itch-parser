cc713f2e873b980c6dc51ada981c283060fbadd6
Yesmurat Sagyndyk <yesmurat2006@gmail.com>
2026-09-10 18:40:12 +0500

    order book test: added test_add_then_replace() test function; all tests pass

--- cpp/book/test_order_book.cpp
+++ cpp/book/test_order_book.cpp
@@ -124,11 +124,60 @@ void test_add_then_delete() {
 
 }
 
+void test_add_then_replace() {
+
+    book::OrderBook new_book;
+    book::BookUpdate captured{};
+
+    bool got_update = false;
+
+    auto callback = [&](const book::BookUpdate& u) {
+        captured = u;
+        got_update = true;
+    };
+
+    new_book.set_callback(callback);
+
+    itch::DecodedMessage message_Add{};
+
+    message_Add.msg_type = 'A';     // message_Add type.
+    message_Add.stock_locate = 10;  // tracking ID of this order.
+    message_Add.field_int[0] = 1;   // order reference.
+
+    message_Add.field_str[1] = 'B'; // buy
+    message_Add.field_int[2] = 300; // 300 shares
+    message_Add.field_int[4] = 45;  // for $45.
+
+    new_book.apply(message_Add);
+    book::BookUpdate captured_after_Add = captured;
+
+    got_update = false;
+
+    itch::DecodedMessage message_Replace{};
+
+    message_Replace.msg_type = 'U';
+
+    message_Replace.field_int[0] = message_Add.field_int[0];
+    message_Replace.field_int[1] = 2;
+    message_Replace.field_int[2] = 130;
+    message_Replace.field_int[3] = 17;
+
+    new_book.apply(message_Replace);
+
+    assert(got_update == true);
+    assert(captured.symbol_index == captured_after_Add.symbol_index);
+    assert(captured.is_buy == captured_after_Add.is_buy);
+    assert(captured.best_shares == message_Replace.field_int[2]);
+    assert(captured.best_price == message_Replace.field_int[3]);
+
+}
+
 int main(int argc, char** argv) {
 
     test_add_order_creates_level();
     test_add_then_cancel_partial();
     test_add_then_delete();
+    test_add_then_replace();
 
     return 0;
 }
